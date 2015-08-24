463b4bc206629528711ddfe752dc74a7c4a3fbcd
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-08-24 22:53:02 +0900

    models updated

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,4 @@
 class Channel < ActiveRecord::Base
 	self.table_name = "channel"
-	has_and_belongs_to_many :locations
+	has_and_belongs_to_many :locations, join_table: 'location_channel'
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ActiveRecord::Base
 	self.table_name = "location"
-
-	has_and_belongs_to_many :channels
+	has_and_belongs_to_many :channels, join_table: 'location_channel'
 end
